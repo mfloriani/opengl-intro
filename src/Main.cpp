11da5ecacbcbb421ee1930ab920c42072c8e21a8
Murilo Floriani <murilo.floriani@gmail.com>
2020-06-10 17:19:38 +0100

    rendering rect using 2 triangles

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,73 +8,128 @@
 const char* APP_TITLE = "Modern OpenGL";
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
-
 bool fullscreen = false;
+bool wireframe = false;
+GLFWwindow* window = nullptr;
+
+const GLchar* vertexShaderSrc =
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;"
+"layout (location = 1) in vec3 color;"
+"out vec3 vert_color;"
+"void main()"
+"{"
+"   vert_color = color;"
+"	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+"}";
+
+const GLchar* fragmentShaderSrc =
+"#version 330 core\n"
+"in vec3 vert_color;"
+"out vec4 frag_color;"
+"void main()"
+"{"
+"	frag_color = vec4(vert_color, 1.0f);"
+"}";
 
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
 void showFPS(GLFWwindow* window);
+bool initOpenGL();
 
 int main()
 {
-	if (!glfwInit())
+	if (!initOpenGL())
 	{
 		std::cerr << "GLFW initialization failed" << std::endl;
-		return -1;
+		return 1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	GLfloat vertices[] = {
+		// position        // color
+		//triangle 0
+		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+	     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+	   //triangle 1
+	   -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+	    0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+	   -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f
+	};
 
-	GLFWwindow* window = nullptr;
-	
-	if (fullscreen)
+	GLuint vbo, vao;
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, NULL);
+	glEnableVertexAttribArray(0);
+
+	//color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, (GLvoid*)(sizeof(GLfloat) * 3));
+	glEnableVertexAttribArray(1);
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	glCompileShader(vs);
+
+	GLint result;
+	GLchar infoLog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+	if (!result)
 	{
-		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-		const GLFWvidmode* vidMode = glfwGetVideoMode(monitor);
-		if (vidMode)
-		{
-			window = glfwCreateWindow(vidMode->width, vidMode->height, APP_TITLE, monitor, NULL);
-		}
-	}
-	else
-	{
-		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APP_TITLE, NULL, NULL);
-	}
-	
-	
-	if (!window)
-	{
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
+		glGetShaderInfoLog(vs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Vertex shader failed to compile" << infoLog << std::endl;
 	}
 
-	glfwMakeContextCurrent(window);
-	
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	glCompileShader(fs);
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+	if (!result)
 	{
-		std::cerr << "GLEW initialization failed" << std::endl;
-		glfwTerminate();
-		return -1;
+		glGetShaderInfoLog(fs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Fragment shader failed to compile" << infoLog << std::endl;
+	}
+	
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Shader program failed to link" << infoLog << std::endl;
 	}
 
-	glfwSetKeyCallback(window, glfw_onKey);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		showFPS(window);
+
 		glfwPollEvents();
 
-		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
 	
-	
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+
 	glfwTerminate();
 
 	return 0;
@@ -85,6 +140,19 @@ void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key == GLFW_KEY_W && action == GLFW_PRESS)
+	{
+		wireframe = !wireframe;
+		if (wireframe)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
 	}
 }
 
@@ -110,4 +178,55 @@ void showFPS(GLFWwindow* window)
 		frameCount = 0;
 	}
 	frameCount++;
+}
+
+bool initOpenGL()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	if (fullscreen)
+	{
+		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* vidMode = glfwGetVideoMode(monitor);
+		if (vidMode)
+		{
+			window = glfwCreateWindow(vidMode->width, vidMode->height, APP_TITLE, monitor, NULL);
+		}
+	}
+	else
+	{
+		window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APP_TITLE, NULL, NULL);
+	}
+
+	if (!window)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "GLEW initialization failed" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwSetKeyCallback(window, glfw_onKey);
+
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+
+	return true;
 }
